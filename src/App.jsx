7aba3aec9987bbb54d76eb216fc66c80bb3af409
Gustavo K <gustavo.kiss@eaportal.org>
2023-08-14 16:23:22 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+
+function App() {
+  
+  const [usuario, setUsuario] = useState("")
+
+  return (
+    <>
+    <header>
+      <h1>Login</h1>    
+    </header>
+
+    <main>
+      <form action="">
+        <div>
+          <label htmlFor="usuario">Usuário</label>
+          <input type="text" id='usuario' placeholder='Digite seu Usuario' />
+        </div>
+        <div>
+          <label htmlFor="senha">Senha</label>
+          <input type="password" id='senha' placeholder='Digite sua senha' />
+        </div>
+        <button>Entrar</button>
+      </form>
+    </main>
+    </>
+  )
+}
+
+export default App
